2fbba49e531e9ed5acc3f28a8b04801b915058b1
neodylan <neodylan@foxmail.com>
2023-05-14 23:07:24 +0800

    craco-less配置

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,15 @@
 const path = require('path')
+const CracoLessPlugin = require('craco-less')
 
 const resolve = (pathname) => path.resolve(__dirname, pathname)
 
 module.exports = {
-  //less
+  // less
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+    },
+  ],
   // webpack
   webpack: {
     alias: {
