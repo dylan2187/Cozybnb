dca1eeb3b26f94200a1ffa6b8194815ed1d3ca4c
neodylan <neodylan@foxmail.com>
2023-05-14 23:33:56 +0800

    CSS样式重置

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React, { memo } from 'react'
-
+import '@/assets/css/reset.less'
 const App = memo(() => {
-  return <div>App</div>
+  return (
+    <div>
+      App
+      <a href="">aribnb</a>
+    </div>
+  )
 })
 
 export default App
