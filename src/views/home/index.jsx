421969f242080e596a20265b3355574f190b901c
neodylan <neodylan@foxmail.com>
2023-05-15 20:36:06 +0800

    封装header标题

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -4,6 +4,7 @@ import { shallowEqual, useDispatch, useSelector } from 'react-redux'
 import { fetchHomeDataAction } from '@/store/modules/home'
 import { HomeWrapper } from './style'
 import HomeBanner from './c-cpns/home-banner'
+import SectionHeader from '@/components/section-header'
 
 const Home = memo(() => {
   // 从redux中获取数据
@@ -21,8 +22,19 @@ const Home = memo(() => {
   }, [dispatch])
   return (
     <HomeWrapper>
-      <h2>{goodPriceInfo.title}</h2>
       <HomeBanner />
+      <div className="good-price">
+        <SectionHeader
+          title={goodPriceInfo.title}
+          subtitle={
+            goodPriceInfo.subtitle ? goodPriceInfo.subtitle : '默认副标题'
+          }></SectionHeader>
+        <ul>
+          {goodPriceInfo?.list?.map((item) => {
+            return <li key={item.id}>{item.name}</li>
+          })}
+        </ul>
+      </div>
     </HomeWrapper>
   )
 })
